ab5a90f182aa21adbcf81750e2f74ea304eb4a34
Jason Savelli <jason.savelli@mention.com>
2024-03-26 13:39:26 +0100

    fix: fix cn utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import clsx from 'clsx'
+import { ClassValue } from 'clsx'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const configuredTwMerge = extendTailwindMerge({
+    extend: {
+        classGroups: {
+            /**
+             * There is an issue with twMerge that conflcits text colors and font sizes
+             * https://github.com/dcastil/tailwind-merge/issues/297#issuecomment-1872546196
+             */
+            'font-size': ['text-b-s', 'text-b-m', 'text-h-s', 'text-h-m'],
+        },
+    },
+})
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return configuredTwMerge(clsx(inputs))
 }
