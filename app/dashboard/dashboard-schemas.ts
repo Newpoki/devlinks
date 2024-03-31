ece6614238c2a163ae96748b4218449bca0f22e9
Jason Savelli <jason.savelli@mention.com>
2024-03-31 18:31:38 +0200

    fix: normalize error messages

--- app/dashboard/dashboard-schemas.ts
+++ app/dashboard/dashboard-schemas.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
 import { DASHBOARD_PLATFORMS_MAPPING } from './dashboard-constants'
+import { PlatformName } from '@prisma/client'
 
 const platformName = z.enum([
     'GITHUB',
@@ -18,123 +19,58 @@ const platformName = z.enum([
     'STACK_OVERFLOW',
 ])
 
+const REQUIRED_FIELD_ERROR_MESSAGE = "Can't be empty"
+
+const INVALID_PLATFORM_URL_ERROR_MESSAGE_PREFIX = 'URL must start with'
+
 const dashboardPlatformOption = z.object({
     id: z.string(),
     name: platformName,
 })
 
-export const dashboardFormValuesSchema = z.object({
-    user: z.object({
-        firstName: z.string(),
-        lastName: z.string(),
-        email: z.string(),
-        id: z.string(),
-    }),
-    platforms: z.array(
+const urlValidator = (platformName: PlatformName) => {
+    const urlPattern = DASHBOARD_PLATFORMS_MAPPING[platformName].urlPattern
+
+    return (
         z
             // This would be better using regexp for this
             // but I think this is a bit too much, and I don't exactly know which chars are allowed
             .object({
                 platformId: z.string(),
-                name: z.literal('GITHUB'),
-                url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.GITHUB.urlPattern),
+                name: z.literal(platformName),
+                url: z
+                    .string()
+                    .min(1, REQUIRED_FIELD_ERROR_MESSAGE)
+                    .startsWith(
+                        urlPattern,
+                        `${INVALID_PLATFORM_URL_ERROR_MESSAGE_PREFIX}: ${urlPattern}`
+                    ),
             })
+    )
+}
 
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('FRONTEND_MENTOR'),
-                    url: z
-                        .string()
-                        .startsWith(DASHBOARD_PLATFORMS_MAPPING.FRONTEND_MENTOR.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('TWITTER'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.TWITTER.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('LINKEDIN'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.LINKEDIN.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('YOUTUBE'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.YOUTUBE.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('FACEBOOK'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.FACEBOOK.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('TWITCH'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.TWITCH.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('DEVTO'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.DEVTO.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('CODEWARS'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.CODEWARS.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('CODEPEN'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.CODEPEN.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('FREECODECAMP'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.FREECODECAMP.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('GITLAB'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.GITLAB.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('HASNODE'),
-                    url: z.string().startsWith(DASHBOARD_PLATFORMS_MAPPING.HASNODE.urlPattern),
-                })
-            )
-            .or(
-                z.object({
-                    platformId: z.string(),
-                    name: z.literal('STACK_OVERFLOW'),
-                    url: z
-                        .string()
-                        .startsWith(DASHBOARD_PLATFORMS_MAPPING.STACK_OVERFLOW.urlPattern),
-                })
-            )
+export const dashboardFormValuesSchema = z.object({
+    user: z.object({
+        firstName: z.string().min(1, REQUIRED_FIELD_ERROR_MESSAGE),
+        lastName: z.string().min(1, REQUIRED_FIELD_ERROR_MESSAGE),
+        email: z.string().min(1, REQUIRED_FIELD_ERROR_MESSAGE).email('Must be a valid email'),
+        id: z.string(),
+    }),
+    platforms: z.array(
+        urlValidator('GITHUB')
+            .or(urlValidator('FRONTEND_MENTOR'))
+            .or(urlValidator('TWITTER'))
+            .or(urlValidator('LINKEDIN'))
+            .or(urlValidator('YOUTUBE'))
+            .or(urlValidator('FACEBOOK'))
+            .or(urlValidator('TWITCH'))
+            .or(urlValidator('DEVTO'))
+            .or(urlValidator('CODEWARS'))
+            .or(urlValidator('CODEPEN'))
+            .or(urlValidator('FREECODECAMP'))
+            .or(urlValidator('GITLAB'))
+            .or(urlValidator('HASNODE'))
+            .or(urlValidator('STACK_OVERFLOW'))
     ),
 })
 
